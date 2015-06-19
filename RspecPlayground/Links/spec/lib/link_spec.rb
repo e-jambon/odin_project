7b28cb5686b471e6c59fb84d680fa6d6999ea49d
e-jambon <admin@e-jambon.com>
2015-06-19 11:57:25 +0200

    Creating context for #valid_url, Going to stub & mock

--- RspecPlayground/Links/spec/lib/link_spec.rb
+++ RspecPlayground/Links/spec/lib/link_spec.rb
@@ -13,26 +13,30 @@ describe Link do
 
     end
 
-    context "given a valid http url", http: true do
-        subject(:link) { Link.new(  {url: "http://links.e-jambon.com" }) }
-          its(:url) { should eq("http://links.e-jambon.com")}
-          it { link.should be_valid_url }
-     end
+    context "#valid_url" do
+    	subject(:link) { Link.new(description: "valid_url_testing link", title: "Testing purpose Link")}
 
-    context "given an invalid url", http: true do
-          subject(:link) { Link.new( {url: "http://tis@not+=validate.thelan" }) }
-        
-          it { link.should_not be_valid_url }
-     end
+	    context "given a valid http url", http: true do
+	        before { link.url=  "http://links.e-jambon.com" } 
+	          its(:url) { should eq("http://links.e-jambon.com")}
+	          it { link.should be_valid_url }
+	     end
 
-	# assuming google.com redirects to your local country version (google.fr for me) 
-	# One might need to change this example for it to work as intended
-	# stup & mocks to the rescue, please.
-    context "given an valid https url, with a redirection", http: true do
-         subject(:link) { Link.new( {url: "https://www.google.com" }) } 
-         its(:url) {should eq("https://www.google.com")}       
-         it { link.should be_valid_url() }
-    end
+	    context "given an invalid url", http: true do
+	          subject(:link) { Link.new({url: "http://tis@not+=validate.thelan" } ) }
+	        
+	          it { link.should_not be_valid_url }
+	     end
+
+		# assuming google.com redirects to your local country version (google.fr for me) 
+		# One might need to change this example for it to work as intended
+		# stup & mocks to the rescue, please.
+	    context "given an valid https url, with a redirection", http: true do
+	         subject(:link) { Link.new( {url: "https://www.google.com" }) } 
+	         its(:url) {should eq("https://www.google.com")}       
+	         it { link.should be_valid_url() }
+	    end
+	end
 
       
 end
