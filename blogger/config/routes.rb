cf34a3a1663bd58eeece5b57a0281a31db78cc71
e-jambon <admin@e-jambon.com>
2015-06-24 13:34:38 +0200

    Adding blogger. Stage II.

--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -53,4 +53,9 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+    resources :articles do
+      resources :comments
+    end
+
+    root to: 'articles#index'
 end
